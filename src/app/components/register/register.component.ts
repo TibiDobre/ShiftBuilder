b2360589d0f55a62ad8719d669a974ffc7bbf107
TibiDobre <tibi.dobre@gmail.com>
2022-04-18 20:58:58 +0300

    register button

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,28 @@
+import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  
+  get username(){
+    let usernameControl = this.regsiterForm.get("username") as FormControl;
+    return usernameControl.value;
   }
 
+    register(){
+      console.log (this.username);
+    }
+  }
 }
