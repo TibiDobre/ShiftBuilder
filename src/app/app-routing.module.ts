9cffb108953d5fa98602c7adcda697fc05c7da88
TibiDobre <tibi.dobre@gmail.com>
2022-04-18 19:30:44 +0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { pathToFileURL } from 'url';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCH6k-cJELajEHNxAdbSWy08mlFIcXzSh4',
+    authDomain: 'shiftbuilder-a4f29.firebaseapp.com',
+    projectId: 'shiftbuilder-a4f29',
+    storageBucket: 'shiftbuilder-a4f29.appspot.com',
+    messagingSenderId: '243016705383',
+    appId: '1:243016705383:web:0f1471d775b4432e02fbc2',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
