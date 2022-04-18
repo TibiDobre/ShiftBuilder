79afea220f0b2c5b8a248ce0305ab2f425af2043
TibiDobre <tibi.dobre@gmail.com>
2022-04-18 19:58:02 +0300

    removed unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { pathToFileURL } from 'url';
+
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
